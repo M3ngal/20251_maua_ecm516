8fce2ffa8df7cad188b6e3536a1f711e5e5f5c53
Leonardo Moura <leohdmoura@gmail.com>
2025-04-05 09:24:18 -0300

    learn(ap01-mss): lida com o codigo 4.3.1

---
+++ microsservicos/lembretes/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+//Um método do protocolo HTTP,
+//Um padrão de acesso,
+//Uma funcionalidade
+//GET /lembretes () => {}
+app.get('/lembretes', (req, res) => {})
